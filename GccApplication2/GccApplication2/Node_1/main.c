2021062cd4f3113c29717c3bc986de81a2baaba8
Ole Mandius Thorrud <125032895+olemthorrud@users.noreply.github.com>
2024-09-12 13:20:08 +0200

    Working system

--- GccApplication2/GccApplication2/Node_1/main.c
+++ GccApplication2/GccApplication2/Node_1/main.c
@@ -22,7 +22,7 @@
 #include "oled.h"
 #include "spi_driver.h"
 #include "mcp_driver.h"
-//#include "can.h" denne ligger nå i adc.h
+//#include "can.h" denne ligger nÃ¥ i adc.h
 
 
 
@@ -37,59 +37,23 @@
 int main(void)
 {
 	
-	// Deklarering av globale verdier for behandling av skjermen og posisjoner på multifunction board
+	// Deklarering av globale verdier for behandling av skjermen og posisjoner pÃ¥ multifunction board
 
 		volatile uint8_t js_pos[4];
 		adc_driver(js_pos);
 		volatile int arrow_pos = 0;
 		int* arrow_pos_ptr = &arrow_pos;
 	
-	// UART
-	
+	//Initialiseringer
 		USART_Init(UBRR);
 		uart_link_printf();
-		
-
-	// Initialize SRAM
-	
 		SRAM_init();
-	
-	
-	// Initialize ADC clock
-	
 		adc_config_clock();
-			
-			
-	// oled
-	
-		oled_init();
-		
-		 //int ADC_driver_data[4];                              //Hvor verdiene i funksjonen under lagres
-		 //multifunction_board_test(ADC_driver_data);           //Testfunskjon for å hente 4 verdier fra js og sliders. Kan nok slås sammen med den under
-		 //oled_test_meny(arrow_pos_ptr,js_pos);              //Testfunskjon til menyen på oledskjermen
-		
-		
-	// Initialisering av can. Initfunksjonen setter den i loopback
-		
-		can_init();
-	
-		// mcp_test();                                        // Test av Spi sender 81 til et register og leser det igjen
-		char my_array[] = {'f',' ','t','e','k','l','e','d'};             //Arrayet som skal sendes i loopbackmode i can_test
-		//can_test(2, 4, my_array);                           //Funksjon som sender innad i can-kontrolleren i loopback mode
-		 
-
-	
-	
+		oled_init();	
+		can_init();	
 		set_normal_mode();
 		
-	//	button_IR_init(); //OLES KODE 
-		
-// 		can_message to_send;
-// 		to_send.id = 2;
-// 		to_send.length = 8;
-// 		memcpy(to_send.data, my_array, 8);
-// 		can_send(&to_send);
-		
+	
 		while(1){
 		//_delay_ms(200);
  			 printf("[");
@@ -99,13 +63,8 @@ int main(void)
  			 				 printf(" , ");
  			 			 }
  			  			 printf("] \r \n");	 
-		send_js_pos(js_pos); 	
-		//printf("I havent stopped yet"); 
-		//send_button_push(); check for this
+		send_js_pos(js_pos);
 		}
-		
-		
-
 	return 0;
 }
 
